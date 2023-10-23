b4dd30a90c407bf2e86ba9275d046027b08f5403
Berecausau <137358146+GabMazo@users.noreply.github.com>
2023-10-23 19:47:18 -0300

    aa
    asda

--- script.js
+++ script.js
@@ -13,14 +13,11 @@ saveProductButton.addEventListener('click', () => {
     const productName = productNameInput.value;
     const productQuantity = productQuantityInput.value;
 
-    // Aqui você pode adicionar o código para salvar o produto em um banco de dados ou em uma estrutura de dados em JavaScript
-
-    // Em seguida, você pode criar um elemento HTML para exibir o produto na lista
     const productItem = document.createElement('div');
     productItem.textContent = `Produto: ${productName}, Quantidade: ${productQuantity}`;
     productList.appendChild(productItem);
 
-    // Limpar os campos do formulário
+   
     productNameInput.value = '';
     productQuantityInput.value = '';
     productForm.style.display = 'none';
