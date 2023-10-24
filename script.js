de1773f8eb3a152df2a6ca6ac98777a819af18bf
Berecausau <137358146+GabMazo@users.noreply.github.com>
2023-10-23 21:24:47 -0300

    sd

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const addProductButton = document.getElementById('add-product');
 const productForm = document.getElementById('product-form');
 const productNameInput = document.getElementById('product-name');
 const productQuantityInput = document.getElementById('product-quantity');
+const productPriceInput = document.getElementById('product-price');
 const saveProductButton = document.getElementById('save-product');
 
 addProductButton.addEventListener('click', () => {
@@ -12,13 +13,15 @@ addProductButton.addEventListener('click', () => {
 saveProductButton.addEventListener('click', () => {
     const productName = productNameInput.value;
     const productQuantity = productQuantityInput.value;
+    const productPrice = productPriceInput.value;
 
 
     const productItem = document.createElement('div');
-    productItem.textContent = `Produto: ${productName}, Quantidade: ${productQuantity}`;
+    productItem.textContent = `Produto: ${productName}, Quantidade: ${productQuantity}, Preço: ${productPrice}`;
     productList.appendChild(productItem);
 
     productNameInput.value = '';
     productQuantityInput.value = '';
+    productPriceInput.value = '';
     productForm.style.display = 'none';
 });
